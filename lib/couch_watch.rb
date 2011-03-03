54f38ddcd7d2c80188b8585400ff2b4dbbfab7bc
mpapis <mpapis@gmail.com>
2011-03-03 13:28:38 +0100

    posts msgs to couch

--- lib/couch_watch.rb
+++ lib/couch_watch.rb
@@ -1,20 +1,22 @@
-# time ruby -e "require 'lib/couch_watch' ; (0..4).each { CouchWatch.new } ; (1..500).each {|i| CouchWatch.add i; Thread.pass }; CouchWatch.flush"
+# time ruby -e "require 'lib/couch_watch' ; CouchWatch.server 'http://localhost:5984/couchwatch/_design/couchwatch/_update/logger' ; (1..1000).each {|i| CouchWatch.add(:debug, \"#{Time.now}, #{i}\"); Thread.pass }; CouchWatch.flush"
 require 'thread'
+require 'net/http'
+require 'uri'
 
 class CouchWatch
   @@mutex = Mutex.new
   @@store = []
   @@loggers = []
+  @@server = nil
 
-  def initialize amount=1
+  def self.worker amount=1
     (0..amount-1).each do
       @@loggers.push(Thread.new do
         loop do
           @@mutex.synchronize do
             if @@store.length > 0
-              #puts " ------- #{@@store*', '}"
-              @@store.each {|message|
-                s = "#{message}2"
+              @@store.each {|severity, message|
+                Net::HTTP.post_form(@@server, { "severity" => severity, "message" => message })
               }
               @@store.clear
             end
@@ -25,14 +27,17 @@ class CouchWatch
     end
   end
 
-  def self.add message
-    CouchWatch.new 2 if @@loggers.length < 1
+  def self.add severity, message
+    CouchWatch.worker 2 if @@loggers.length < 1
     @@mutex.synchronize do
-      @@store.push message
+      @@store.push [severity, message]
     end
   end
   def self.flush
     #TODO: replace dead threads with new ones
     @@loggers[0].run if @@loggers[0]
+  end
+  def self.server server
+    @@server = URI.parse(server)
   end
 end
